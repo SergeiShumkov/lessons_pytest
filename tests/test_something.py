2a0b8f901c74ae6e7f4bfb3404f00606873a3877
SergeiShumkov <sergeishumkov72@yahoo.com>
2022-09-18 21:25:26 +0500

    18.09.2022

--- tests/test_something.py
+++ tests/test_something.py
@@ -1,18 +1,21 @@
 import requests
 
+from src.schemas.user import User
 from configuration import SERVICE_URL
-# from src.schemas.post import POST_SCHEMA
-from src.pydantic_schema.post import Post
 from src.baseclasses.response import Response
 
 
+resp = requests.get(SERVICE_URL)
 
-def test_getting_posts():
-    r = requests.get(url=SERVICE_URL)
-    response = Response(r)
 
-    response.assert_status_code(200).validate(Post)
+# def test_1():
+#     # print(resp.json())
+#     # print(resp.__getstate__())
+#     print("\n ", resp.url, "\n ", resp.encoding)
 
-    # response.assert_status_code(200).validate(POST_SCHEMA)    
 
-# [{'id': 1, 'title': 'Post 1'}, {'id': 2, 'title': 'Post 2'}, {'id': 3, 'title': 'Post 3'}]
+
+def test_getting_users_list():
+    response = requests.get(SERVICE_URL)
+    test_object = Response(response)
+    test_object.assert_status_code(300).validate(User)
