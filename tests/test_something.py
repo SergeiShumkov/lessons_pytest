6b38b1359117e976affdb4c63a4bebff4f6ceeb5
SergeiShumkov <sergeishumkov72@yahoo.com>
2022-09-20 08:11:42 +0500

    20.09.2022

--- tests/test_something.py
+++ tests/test_something.py
@@ -7,15 +7,16 @@ from src.baseclasses.response import Response
 
 resp = requests.get(SERVICE_URL)
 
-
 # def test_1():
 #     # print(resp.json())
 #     # print(resp.__getstate__())
 #     print("\n ", resp.url, "\n ", resp.encoding)
 
-
-
 def test_getting_users_list():
     response = requests.get(SERVICE_URL)
     test_object = Response(response)
-    test_object.assert_status_code(300).validate(User)
+    test_object.assert_status_code(200).validate(User)
+    # print(say_hello)
+
+def test_another():
+    assert 1 == 1
