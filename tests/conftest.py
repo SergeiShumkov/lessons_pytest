6883f79a6ffbc7d99b41c5668e0c9a0b242c2988
SergeiShumkov <sergeishumkov72@yahoo.com>
2022-09-22 13:17:28 +0500

    5

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,10 @@ def get_number():
     return randrange(1, 1000, 5)
 
 def _calculate(a, b):
-    return a + b
+    if isinstance(a, int) and isinstance(b, int):
+        return a + b
+    else:
+        return None
 
 @pytest.fixture
 def calculate():
@@ -15,7 +18,7 @@ def calculate():
 
 @pytest.fixture
 def make_number():
-    print("\nI'm getting number")
+    # print("\nI'm getting number")
     number = randrange(1, 1000, 5)
     yield number
-    print(f"\nNumber at home {number}")
+    # print(f"\nNumber at home {number}")
--- tests/users/test_users.py
+++ tests/users/test_users.py
@@ -1,3 +1,5 @@
+import pytest
+
 from src.schemas.user import User
 from src.baseclasses.response import Response
 
@@ -9,10 +11,39 @@ from src.baseclasses.response import Response
 
 def test_getting_users_list(get_users, make_number):  # (get_users, calculate)
     Response(get_users).assert_status_code(200).validate(User)
-    print(make_number)
+    # print(make_number)
     
     # print(calculate (1, 1))
 
-
+@pytest.mark.development  # см. pytest.ini
+@pytest.mark.production
+@pytest.mark.skip('[ISSUE-23414] Issue with network connection')   # тест не будет выполняться
 def test_another():
     assert 1 == 1
+
+# def test_calculation_both_negative(calculate):
+#     print(calculate(-1, -2))
+
+# def test_calculation_one_negative(calculate):
+#     print(calculate(-1, 2))
+
+# def test_calculation_both_positive(calculate):
+#     print(calculate(1, 2))
+
+# def test_calculation_one_char(calculate):
+#     print(calculate('a', -2))
+
+# def test_calculation_two_chars(calculate):
+#     print(calculate('a', 'b'))
+
+@pytest.mark.development # pytest -s -v -k development tests/users/test_users.py
+                         #  pytest -s -v -k " not development" tests/users/test_users.py
+@pytest.mark.parametrize('first_value, second_value, result', [
+   (1, 2, 3),
+   (-1, -2, -3),
+   (-1, 2, 1),
+   ('b', -2, None),
+   ('b', 'b', None)
+])
+def test_calculator(first_value, second_value, result, calculate): 
+    assert calculate(first_value, second_value) == result
